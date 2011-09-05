78d5c523058795e4a84ae342571e44d1e018f44d
Andrew Clunis <andrew@orospakr.ca>
2011-09-05 11:49:45 -0400

    Get MultiverseID, determine if we're on the last page of the results or not.

--- cardscience.js
+++ cardscience.js
@@ -5,12 +5,14 @@ var cradle = require("cradle");
 
 var zombie = require("zombie");
 
+var url = require('url');
+
 var Scraper = function() {
     // http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&output=standard&special=true&format=%5B%22Standard%22%5D
 
     var browser = new zombie.Browser({debug: true});
     browser.runScripts = false;
-    browser.visit("http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&output=standard&special=true&format=%5B%22Standard%22%5D", function(err, browser, status) {
+    browser.visit("http://gatherer.wizards.com/Pages/Search/Default.aspx?page=60&output=standard&special=true&format=%5B%22Standard%22%5D", function(err, browser, status) {
         if(err) {
             console.log("Shit, couldn't fetch page from Gatherer.");
             // TODO if it failed, try again at least a few times before emitting error
@@ -28,8 +30,22 @@ var Scraper = function() {
         var card_items = verifyElements(card_item_table.querySelectorAll("tr.cardItem"));
         card_items.update().forEach(function(card_item) {
             var title = card_item.querySelector("span.cardTitle a").innerHTML;
-            console.log(title);
+            var set_versions = verifyElements(card_item.querySelector("td.setVersions"));
+            var set_hyperlink_element = verifyElements(set_versions.querySelector('div[id$="cardSetCurrent"] > a'));
+            var set_hyperlink = url.parse(set_hyperlink_element.href, true);
+            var mid = set_hyperlink.query.multiverseid;
+            console.log(mid + ": " + title);
         });
+
+        var page_links = browser.document.querySelector('div.pagingControls')
+
+        var second_last = page_links.childNodes[page_links.childNodes.length - 1];
+
+        if(second_last.tagName === "A") {
+            console.log("More to do!");
+        } else {
+            console.log("On last page!");
+        }
     });
 };
 
